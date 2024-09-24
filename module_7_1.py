e59d2f2577ddf4ac452f368c8c3e016bcb29bcd8
BerellExpress <yury1997@mail.ru>
2024-09-24 13:50:57 +0300

    Add files via upload

--- module_7_1.py
+++ module_7_1.py
@@ -1,38 +1,49 @@
+from itertools import product
+
+
 class Product:
+
     def __init__(self, name, weight, category):
-        self.name = str(name)
-        self.weight = float(weight)
-        self.category = str(category)
+        self.name = name
+        self.weight = weight
+        self.category = category
 
     def __str__(self):
-        return print(f'{self.name}, {self.weight}, {self.category}')
+        return f"{self.name}, {self.weight}, {self.category}"
 
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
 
-class Shop(Product):
+    def file_name(self):
+        return self.__file_name
 
-    __file_name = 'products.txt'
-    def __init__(self, Shop):
-        self.Shop = Shop()
-    def get_products(self, __file_name):
-        file = open(__file_name, 'r')
-        file.close()
-        return __file_name
+    def get_products(self):
+        with open(self.__file_name, mode='r', encoding='utf8') as file:
+            file_products = file.read()
+        return file_products
 
     def add(self, *products):
-        file = open(self.__file_name, 'a')
-        if products in self.__file_name:
-            print(f'\nПродукт {self.name} уже есть в магазине')
-        else:
-            file.write(products)
-        file.close()
+        with open(self.__file_name, mode='r', encoding='utf8') as file:
+            line = True
+            for line in file:
+                self.get_products()
+                if str(products) in line:
+                    print(f'Продукт {products} уже есть в магазине')
+                    break
+                else:
+                    file.write(f'{products}\n')
 
-s1 = Shop()
+
+
+shop = Shop()
+
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2) # __str__
+print(p2)
 
-s1.add(p1, p2, p3)
+shop.add(p1, p2, p3)
 
-print(s1.get_products())
+print(shop.get_products())
